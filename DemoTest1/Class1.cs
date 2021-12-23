e5050c0855f894f5a3a86d8c456730c4a0906b2a
HP <HP@LAPTOP-143FVQFR>
2021-12-23 08:53:23 +0530

    Add project files.

---
+++ DemoTest1/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoTest1
+{
+    public class Class1
+    {
+       
+          /*  {
+
+            int count = 0;
+        string str = "ALPHA";
+            string temp = "";
+
+        for(int i=0;i<str.Length;i++)
+            {
+                for(int j=i+1;j<str.Length;j++)
+                if(str[i]==str[j])
+                    { count++;
+                        temp = temp + str[i] + ":" + count;
+                    }
+                    else
+                    {
+
+                    }*/
+                
+               public void sum()
+                {
+
+                }
+                 
+
+    }
+}
---
+++ DemoTest1/Program1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Write a program which will take user input in string format and prints no of occurrences of all unique characters? For example:
+Input- ALPHA
+Output - A:2 L:1 P:1 H:1*/
+
+namespace DemoTest1
+{
+    internal class Program1
+    {
+        public  static void Main(String[] args)
+        {
+            Class1 s=new Class1();
+            s.sum();
+
+            
+           
+
+        }
+    }
+}
